9df03cac9a6d00f29609c3862cb5323732ff282d
MaxMelnik1311 <maaxfresh1100@gmail.com>
2020-02-19 01:29:18 +0200

    Updates

---
+++ precache-manifest.68baa4ef1d7252df26fbd755246f20e9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9b5c1265f1333e7c64179bff42c322e",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "2da556117ff40bc9f421",
+    "url": "/goit-react-hw-02-feedback/static/js/2.1d4815ed.chunk.js"
+  },
+  {
+    "revision": "a8727e10ec5df36ae832",
+    "url": "/goit-react-hw-02-feedback/static/js/main.0b6f33a0.chunk.js"
+  },
+  {
+    "revision": "724d9f077e212c444ebb",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.6100aeb1.js"
+  }
+]);
