095cd6ee3fb56d0f495a6257ad87fdba9064558d
MaxMelnik1311 <maaxfresh1100@gmail.com>
2020-02-22 10:53:50 +0200

    added .map() in FeedbackOptions

--- src/components/Section/FeedbackOptions.js
+++ src/components/Section/FeedbackOptions.js
@@ -1,28 +1,19 @@
 import React from 'react';
 import T from 'prop-types';
-// import shortid from 'shortid';
+import shortid from 'shortid';
 
 function FeedbackOptions({ options, onLeaveFeedback }) {
-  // const buttonBar = options.map(option => (
-  //   <button
-  //     key={shortid.generate()}
-  //     type="button"
-  //     onClick={() => onLeaveFeedback({ option })}
-  //   >
-  //     {option}
-  //   </button>
-  // ));
   return (
     <div>
-      <button type="button" onClick={() => onLeaveFeedback(options[0])}>
-        {options[0]}
-      </button>
-      <button type="button" onClick={() => onLeaveFeedback(options[1])}>
-        {options[1]}
-      </button>
-      <button type="button" onClick={() => onLeaveFeedback(options[2])}>
-        {options[2]}
-      </button>
+      {options.map(option => (
+        <button
+          key={shortid.generate()}
+          type="button"
+          onClick={() => onLeaveFeedback(option)}
+        >
+          {option}
+        </button>
+      ))}
     </div>
   );
 }
